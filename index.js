1fb649185b37b9708598181836ee6226b7145add
shanxin <shanxin@youchain.cc>
2019-07-24 11:19:01 +0800

    add style

--- index.js
+++ index.js
@@ -1,7 +1,81 @@
 module.exports = {
-	hooks: {},
+  
+  // Extend website resources and html
+    website: {
+        assets: "./assets/",
+        js: [
+            "demo.js"
+        ],
+        css: [
+            "demo.css"
+        ],
+        html: {
+            "html:start": function() {
+                return "<!-- Start book "+this.options.title+" -->"
+            },
+            "html:end": function() {
+                return "<!-- End of book "+this.options.title+" -->"
+            },
 
-	blocks: {},
-	
-	filters: {}
+            "head:start": "<!-- head:start -->",
+            "head:end": "<!-- head:end -->",
+
+            "body:start": "<!-- body:start -->",
+            "body:end": "<!-- body:end -->"
+        }
+    },
+
+    // Extend ebook resources and html
+    website: {
+        assets: "./assets/",
+        js: [
+            "demo.js"
+        ],
+        css: [
+            "demo.css"
+        ],
+        html: {
+            "html:start": function() {
+                return "<!-- Start book "+this.options.title+" -->"
+            },
+            "html:end": function() {
+                return "<!-- End of book "+this.options.title+" -->"
+            },
+
+            "head:start": "<!-- head:start -->",
+            "head:end": "<!-- head:end -->",
+
+            "body:start": "<!-- body:start -->",
+            "body:end": "<!-- body:end -->"
+        }
+    },
+
+  // build 时绑定
+  hooks: {
+  	"init": function(){
+  		console.log("init!");
+  	},
+
+  	"finish:before": function(){
+  		console.log("finish:before");
+  	},
+
+  	"finish": function(){
+  		console.log("finish");
+  	}
+  },
+
+  blocks: {
+  	myTag: {
+  	  process: function(blk){
+  	    return "Hello" + blk.body;
+  	  }
+  	}
+  },
+  
+  filters: {
+  	myFilter: function(s){
+  		return "Hello "+s;
+  	}
+  }
 };
